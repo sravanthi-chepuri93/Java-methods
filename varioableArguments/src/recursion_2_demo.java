81ca36dcd755de9331efe4e0303736b738851105
sravanthi-chepuri93 <sravanthi.chepuri93@gmail.com>
2023-06-21 23:47:59 -0400

    recursion demo-practice

---
+++ varioableArguments/src/recursion_2_demo.java
@@ -0,0 +1,12 @@
+public class recursion_2_demo {
+    static void fun(int n){
+        if(n>0){
+            fun(n-1);
+            System.out.println(n);
+        }
+    }
+    public static void main(String[] args){
+        fun(3);
+
+    }
+}
